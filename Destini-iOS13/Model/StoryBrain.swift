a5f8c889c14707342c561f700ef3f8e893ad515b
Mweh <fahmianton@gmail.com>
2022-08-02 09:28:22 +0800

    level easy

--- Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,21 @@
 
 import Foundation
 
-
+struct StoryBrain{
+    var stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+                   Story(title: "You see tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+                   Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps."),
+    ]
+    
+    var storyNumber = 0
+    
+    mutating func nextStory(_ userChoice: String){
+        if userChoice == stories[0].choice1{
+            storyNumber = 1
+        }
+        if userChoice == stories[0].choice2{
+            storyNumber = 2
+        }
+    }
+    
+}
